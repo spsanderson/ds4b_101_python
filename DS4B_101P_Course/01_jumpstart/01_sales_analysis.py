dddd2869f47ca549375bf080fed06f50b786c790
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-07-27 14:22:54 -0400

    Create 01_sales_analysis.py

---
+++ DS4B_101P_Course/01_jumpstart/01_sales_analysis.py
@@ -0,0 +1,119 @@
+# DS4B 101-P: PYTHON FOR DATA SCIENCE AUTOMATION ----
+# JUMPSTART (Module 1): First Sales Analysis with Python ----
+
+# Important VSCode Set Up:
+#   1. Select a Python Interpreter: ds4b_101p
+#   2. Delete terminals to start a fresh Python Terminal session
+
+# 1.0 Load Libraries ----
+
+# Core Python Data Analysis
+
+
+# Plotting
+
+
+# Misc
+
+
+
+# 2.0 Importing Data Files ----
+
+# help(pd.read_excel)
+# - Use "q" to quit
+
+
+
+
+
+# 3.0 Examining Data ----
+
+
+
+# 4.0 Joining Data ----
+
+
+
+# 5.0 Wrangling Data ----
+
+# * No copy
+
+
+# * Copy
+
+
+
+# * Handle Dates
+
+
+# * Show Effect: Copy vs No Copy
+
+
+# * Text Columns
+
+
+# * Splitting Description into category_1, category_2, and frame_material
+
+
+
+# * Splitting Location into City and State
+
+
+# * Price Extended
+
+
+# * Reorganizing
+
+
+# * Renaming columns
+
+
+# 6.0 Visualizing a Time Series ----
+
+
+# 6.1 Total Sales by Month ----
+
+
+# Quick Plot ----
+
+
+# Report Plot ----
+
+
+
+# 6.2 Sales by Year and Category 2 ----
+
+# ** Step 1 - Manipulate ----
+
+
+# Step 2 - Visualize ----
+
+
+# Simple Plot
+
+
+# Reporting Plot
+
+
+
+# 7.0 Writing Files ----
+
+
+# Pickle ----
+
+
+# CSV ----
+
+
+# Excel ----
+
+
+# WHERE WE'RE GOING
+# - Building a forecast system
+# - Create a database to host our raw data
+# - Develop Modular Functions to:
+#   - Collect data
+#   - Summarize data and prepare for forecast
+#   - Run Automatic Forecasting for One or More Time Series
+#   - Store Forecast in Database
+#   - Retrieve Forecasts and Report using Templates
